e99ac9f05b72db05f050759068c2c2ad8376484f
DPops99 <DanijelPopovic@fer.hr>
2022-05-25 16:17:46 +0200

    finger detection fixed

--- fingertip_detection/detection.py
+++ fingertip_detection/detection.py
@@ -8,79 +8,6 @@ from RefineNet_model.train import *
 from PIL import Image
 import torch.nn.functional as F
 
-def get_image_with_contours(image):
-    print(f"image: {image}")
-    gray = image.copy()
-
-    gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
-    # print(f"gray: {gray.shape}")
-
-    contours, hierarchy = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    print(f"len of contours: {len(contours)}")
-    hull_list = []
-    largest_contour = contours[0]
-    for index, contour in enumerate(contours):
-        hull = cv2.convexHull(contour)
-        hull_list.append(hull)
-        if cv2.contourArea(largest_contour) < cv2.contourArea(contour):
-            largest_contour = contour
-
-    largest_hull = cv2.convexHull(largest_contour)
-    largest_hull_indicies = cv2.convexHull(largest_contour, returnPoints=False)
-    defects = cv2.convexityDefects(largest_contour, largest_hull_indicies)
-    print(f"len of defects: {len(defects)}")
-    # all_hulls_mask = mask.copy()
-    # all_contours_mask = mask.copy()
-    # largest_contour_mask = mask.copy()
-    # largest_hull_mask = mask.copy()
-
-    # img_show = cv2.cvtColor(img.copy(), cv2.COLOR_GRAY2BGR)
-
-    cv2.drawContours(image, [largest_contour], -1, (0, 255, 0), 3)
-    cv2.drawContours(image, [largest_hull], -1, (0, 0, 255), 3)
-
-
-
-    # cv2.imwrite(hull_path, img)
-    for defect in defects:
-        start_index, end_index, farthest_index, depth = defect[0]
-
-        start_point = tuple(largest_contour[start_index][0])
-        end_point = tuple(largest_contour[end_index][0])
-        farthest_point = tuple(largest_contour[farthest_index][0])
-        triangle_area = cv2.contourArea(np.array([start_point, end_point, farthest_point]))
-        if triangle_area > 1000:
-            print(f"triangle_area: {triangle_area}")
-            cv2.circle(image, farthest_point, 5, (255, 0, 0), -1)
-
-    # cv2.imwrite(hull_points_path, img)
-    # for hull in largest_hull:
-    #     print(f"hull: {hull[0]}")
-    #     cv2.circle(img, hull[0], 5, (255, 0, 0), -1)
-
-    print(f"defects shape: {defects.shape}")
-    print(f"defects shape[0]: {defects.shape[0]}")
-
-    cv2.imshow('image_final', image)
-    cv2.waitKey(0)
-    return
-
-    # cv2.drawContours(largest_hull_mask, [largest_hull], -1, (0, 0, 255), 3)
-    # cv2.drawContours(all_contours_mask, contours, -1, (0, 0, 255), 3)
-    # cv2.drawContours(all_hulls_mask, hull_list, -1, (0, 0, 255), 3)
-
-    # cv2.imshow("largest_contour_mask",largest_contour_mask)
-    # cv2.imshow("largest_hull_maskk", largest_hull_mask)
-    # cv2.imshow("all_contours_mask", all_contours_mask)
-    # cv2.imshow("all_hulls_mask", all_hulls_mask)
-
-    # cv2.imshow("img_show", img)
-
-    # cv2.waitKey(0)
-
-    # cv2.drawContours(mask, [largest_hull], -1, (0, 0, 255), 3)
-    # cv2.imshow("hull",mask)
-    # cv2.waitKey(0)
 
 def get_image(image_path):
     return cv2.imread(image_path)
@@ -100,24 +27,6 @@ def get_hand_center(mask):
     center_Y = int(M["m01"] / M["m00"])
 
     print(np.array(largest_contour).shape)
-    # largest_hull = cv2.convexHull(largest_contour)
-    # print(mask.shape)
-    # print(type(mask))
-    # print(mask.shape)
-    # blank_array = np.zeros(mask.shape)
-    # cv2.drawContours(blank_array, [largest_contour], -1, (0, 255, 0), 3)
-    # cv2.drawContours(mask_rgb, [largest_contour], -1, (0, 255, 0), 3)
-    # cv2.drawContours(blank_array, [largest_hull], -1, (0, 0, 255), 3)
-    # cv2.drawContours(mask_rgb, [largest_hull], -1, (0, 0, 255), 3)
-    # cv2.circle(blank_array, (center_X, center_Y), 1, (255, 255, 255), -1)
-    # cv2.circle(mask_rgb, (center_X, center_Y), 7, (255, 255, 255), -1)
-    #
-    #
-    # print(blank_array.shape)
-    # cv2.imshow('blank',blank_array)
-    # cv2.imshow('mask', mask_rgb)
-    # cv2.waitKey(0)
-
     return (center_X,center_Y)
 
 def get_contour(mask):
@@ -146,6 +55,7 @@ def get_defects(contour, hull):
 
 def get_far_points(contour, defects):
     far_points = []
+    far_indicies = []
     for i in range(defects.shape[0]):
         s, e, f, d = defects[i, 0]
         start = tuple(contour[s][0])
@@ -155,7 +65,8 @@ def get_far_points(contour, defects):
         if triangle_area > 1000:
             print(f"triangle_area: {triangle_area}")
             far_points.append(far)
-    return far_points
+            far_indicies.append(f)
+    return far_points, far_indicies
 
 def get_hand_center(contour):
     M = cv2.moments(contour)
@@ -169,54 +80,56 @@ def signature(mask):
     contour, contours, hierarchy = get_contour(mask)
     hull = get_hull(contour)
     defects = get_defects(contour,hull)
-    far_points = get_far_points(contour, defects)
+    far_points, far_indicies = get_far_points(contour, defects)
     hand_center = get_hand_center(contour)
     distances = []
     distance = []
     all_dist = []
     hull_distances = []
+    fingertip_indicies = []
     fingertip_points = []
-    #SEPERATE CONTOURS BY DEFECT POINTS
-    for point in contour:
-        distance.append(math.dist(hand_center, point[0]))
-        all_dist.append(math.dist(hand_center, point[0]))
-        for far_point in far_points:
-            if np.array_equal(far_point, point[0]):
-                distances.append(distance)
-                # max_index = get_max_value_index(distance)
-                # for dist in distances[:-1]:
-                #     max_index += len(dist)
-                # fingertip = contour[max_index]
-                # fingertip_points.append(fingertip)
-                distance = []
+
+    # #SEPERATE CONTOURS BY DEFECT POINTS
+    # for index, point in enumerate(contour):
+    #     distance.append(math.dist(hand_center, point[0]))
+    #     # all_dist.append(math.dist(hand_center, point[0]))
+    #     for far_point in far_points:
+    #         if np.array_equal(far_point, point[0]):
+    #             distances.append(distance)
+    #             # max_index = get_max_value_index(distance)
+    #             # for dist in distances[:-1]:
+    #             #     max_index += len(dist)
+    #             # fingertip = contour[max_index]
+    #             # fingertip_points.append(fingertip)
+    #             distance = []
+    #             fingertip_indicies.append(index)
+    #             break
+
+
+    distances.append(distance)
+    real_finger_contours = get_finger_contours(contour=contour, far_indicies=far_indicies)
+    fixed_finger_contours = fix_finger_contours(finger_contours=real_finger_contours)
+    real_distences = get_finget_contour_dist(hand_center=hand_center, finger_contours=fixed_finger_contours)
+    real_fingertips = get_fingertips(finger_contour_dist=real_distences, finger_contour=fixed_finger_contours)
+    real_fingertip_indicies = get_fingertip_indicies(fingertips=real_fingertips, contour=contour)
+
     print(fingertip_points)
+    print(real_fingertips)
+    # print(distances)
+    # max_point = contour[np.argmax(distances)]
+    # print(max_point)
+    # fig, axs = plt.subplots(len(distances))
+    # for index, dist in enumerate(distances):
+    #     x = [i for i in range(len(dist))]
+    #     axs[index].plot(x,dist)
+    # plt.show()
+    # plt.clf()
 
-    # #CUT THE LARGEST DISTANCE CONTOUR INTO 3 PARTS
-    longest_dist = get_longest_distance(distances)
-    size = int(len(longest_dist)*0.33)
-    distances.remove(longest_dist)
-    distances.append(longest_dist[:size])
-    distances.append(longest_dist[-size:])
-
-    for index, dist in enumerate(distances):
-        max_index = get_max_value_index(dist)
-        for prev_dist in distances[:index]:
-            max_index += len(prev_dist)
-        if index > 2:
-            max_index += size
-        fingertip = contour[max_index]
-        fingertip_points.append(fingertip)
-
-
-
-    print(distances)
-    max_point = contour[np.argmax(distances)]
-    print(max_point)
-    fig, axs = plt.subplots(len(distances))
-    for index, dist in enumerate(distances):
-        x = [i for i in range(len(dist))]
-        axs[index].plot(x,dist)
-    plt.show()
+    # fig, axs = plt.subplots(len(real_distences))
+    # for index, dist in enumerate(real_distences):
+    #     x = [i for i in range(len(dist))]
+    #     axs[index].plot(x, dist)
+    # plt.show()
 
 
     cv2.drawContours(mask, [contour], -1, (0, 255, 0), 3)
@@ -224,7 +137,7 @@ def signature(mask):
     cv2.circle(mask, hand_center, 1, (0, 0, 255), -1)
     for point in far_points:
         cv2.circle(mask, point, 3, (0, 0, 255), -1)
-    for point in fingertip_points:
+    for point in real_fingertips:
         cv2.circle(mask, point[0], 3, (255, 0, 0), -1)
     # cv2.circle(mask, max_point[0], 5, (255, 0, 0), -1)
     # for i in range(len(contours)):
@@ -232,7 +145,7 @@ def signature(mask):
     #     cv2.drawContours(mask, contours, i, color, 2, cv2.LINE_8, hierarchy, 0)
     cv2.imshow('mask',mask)
     image_real = get_image('/home/popa/Documents/diplomski_rad/FingertipDetectionAndTrackingForPatternRecognitionOfAirWritingInVideos/fingertip_detection/runs/detect/exp3/crops/hand/2022-01-19-215301_3.jpg')
-    cv2.circle(image_real, max_point[0], 5, (255, 0, 0), -1)
+    # cv2.circle(image_real, max_point[0], 5, (255, 0, 0), -1)
     cv2.imshow('image_real',image_real)
     cv2.waitKey()
 
@@ -241,6 +154,58 @@ def get_longest_distance(distances):
 
 def get_max_value_index(distances):
     return np.argmax(distances)
+
+def fix_finger_contours(finger_contours):
+    longest_contour = get_longest_distance(finger_contours)
+    finger_contours.remove(longest_contour)
+    size = int(len(longest_contour)*0.33)
+    finger_contours.append(longest_contour[:size])
+    finger_contours.append(longest_contour[-size:])
+    return finger_contours
+
+def get_finger_contours(contour, far_indicies):
+    finger_contours = []
+    for index in range(len(far_indicies)-1):
+        current_far_index = far_indicies[index]
+        next_far_index = far_indicies[index+1]
+        finger_contours.append(contour[current_far_index:next_far_index])
+
+    last_index = far_indicies[-1]
+    # last_contour = []
+    # last_contour = list(contour[last_index:])
+    # last_contour.extend(contour[:far_indicies[0]])
+    first = contour[last_index:]
+    last = contour[:far_indicies[0]]
+    last_contour = np.concatenate((contour[last_index:], contour[:far_indicies[0]]))
+    finger_contours.append(last_contour)
+
+    return finger_contours
+
+def get_finget_contour_dist(hand_center, finger_contours):
+    contour_dist = []
+    for finger in finger_contours:
+        current_dist = []
+        for point in finger:
+            current_dist.append(math.dist(hand_center, point[0]))
+        contour_dist.append(current_dist)
+    return contour_dist
+
+def get_fingertips(finger_contour_dist, finger_contour):
+    fingertips = []
+    for finger_dist, finger in zip(finger_contour_dist, finger_contour):
+        contour_dist_index = np.argmax(finger_dist)
+        fingertips.append(finger[contour_dist_index])
+    return fingertips
+
+def get_fingertip_indicies(fingertips, contour):
+    fingertip_indicies = []
+    for index, point in enumerate(contour):
+        for finger_point in fingertips:
+            if np.array_equal(finger_point[0], point[0]):
+                fingertip_indicies.append(index)
+
+    return fingertip_indicies
+
 
 def get_yolo_pic():
     model_path = '/home/popa/Documents/diplomski_rad/FingertipDetectionAndTrackingForPatternRecognitionOfAirWritingInVideos/yolov5_best_model/best.pt'
